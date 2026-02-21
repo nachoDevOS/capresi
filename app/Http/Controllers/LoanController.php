1d6c322fba50fea3638a4560a2f6a52a6aba9416
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2026-02-21 18:58:16 -0400

    dev

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -984,7 +984,7 @@ class LoanController extends Controller
         }
 
         // Calculamos el tiempo tentativo (cola secuencial + delay aleatorio)
-        $sendAt1 = $lastScheduled->copy()->addMinutes(rand(5, 10));
+        $sendAt1 = $lastScheduled->copy()->addMinutes(rand(2, 10));
 
         // --- ENVÍO 1: Saludo ---
         Cache::put('last_whatsapp_schedule', $sendAt1, now()->addDay());
